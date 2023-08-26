ac50ac2e30910bb001baaa07b4f0d8d5f086a9ae
Luciano <lucluci@hotmail.com>
2023-08-26 12:33:52 -0300

    usando metodos

--- sistemaSmartTv/src/sistemaSmartTv/Usuario.java
+++ sistemaSmartTv/src/sistemaSmartTv/Usuario.java
@@ -5,8 +5,13 @@ public class Usuario {
 	public static void main(String[] args) {
 
 		SmartTv smartTv = new SmartTv();
-		
-		
+		SmartTv minhaSmart = new SmartTv();
+	
+		System.out.println("minha nova tv esta ligada? " + minhaSmart.ligada);
+		minhaSmart.ligar();
+		System.out.println("agora minha nova tv esta ligada? " + minhaSmart.ligada);
+		minhaSmart.diminuirVolume();
+				
 		smartTv.diminuirVolume();
 		smartTv.diminuirVolume();
 		smartTv.diminuirVolume();
@@ -19,6 +24,8 @@ public class Usuario {
 		System.out.println("Canal atual ? " + smartTv.canal);
 		System.out.println("Volume atual ? " + smartTv.volume);
 		System.out.println(smartTv.somar(30,40));
+		
+		
 		
 		
 		
